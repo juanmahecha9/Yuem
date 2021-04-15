50832f499d4ecc6566baec0782befef21ce1d9c9
juanmahecha9 <juanmahecha.dc@gmail.com>
2021-04-15 00:27:14 -0500

    Agregar interfaz grafica HTML y CSS al archivo de configuración

--- nodeInit.sh
+++ nodeInit.sh
@@ -1,71 +1,91 @@
 #!/bin/bash
 set -fueo pipefail
-#!/bin/bash
+
 # Ingreso del nombre del proyecto
 echo 'Project Name:'
 read projectName
 mkdir $projectName
 
 echo "
- EXECUTING:                                             
- __   __  _   _   _____   __  __     
- \ \ / / | | | | | ____| |  \/  |    
-  \ V /  | | | | |  _|   | |\/| |    
-   | |   | |_| | | |___  | |  | |  _ 
+ EXECUTING:
+ __   __  _   _   _____   __  __
+ \ \ / / | | | | | ____| |  \/  |
+  \ V /  | | | | |  _|   | |\/| |
+   | |   | |_| | | |___  | |  | |  _
    |_|    \___/  |_____| |_|  |_| (_)
-                                     
+
 "
 # Dealy de carga
-sleep 5
+sleep 1
 
 cd $projectName
 npm init -y
 npm i express morgan path cors nodemailer && npm i -D nodemon
 npm audit
-mkdir scr
-cd scr
+mkdir src
+cd src
 mkdir views public private helpers routes
 cd ..
 
+#Modificar archivo package.json para ingresar los scripts
+
 #Crear archivo de configuración index.js dentro de la carpeta principal del proyecto
-touch index.js && echo " // Configuración rapida de proyecto Node.js by juanmahecha9
+touch index.js && echo "
+// Configuración rapida de proyecto Node.js by juanmahecha9
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+
+const router = require('./src/routes/routes.js')
 
 const app = express();
 
 app.set('port', process.env.PORT || 4000);
 
-app.use(express.static(path.join(__dirname, 'src/views')));
+// Load static files
+app.use(express.static(path.join(__dirname, '/src/views')));
+app.use(express.static(path.join(__dirname, '/src/public')));
+
 app.use(express.urlencoded({ extended: false }));
 
 app.use(morgan('dev'));
+app.use(router);
+
 app.listen(app.get('port'),()=>{
 console.log('server running at http://localhost:'+app.get('port'));
-})" > index.js
+})
+" > index.js
 
 #Crear archivos HTML y CSS del proyecto
-cd scr/views && touch index.html && echo "
+cd src/views && touch index.html && echo "
+
 <!DOCTYPE html>
 <html lang='en'>
 
 <head>
-    <meta charset='UTF-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <link rel='stylesheet' href='./public/index.css'>
+    <meta charset='UTF-8' />
+    <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+    <meta name='viewport' content='width=device-width, initial-scale=1.0' />
     <!-- Bootstrap styles -->
     <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css' rel='stylesheet'
-        integrity='sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6' crossorigin='anonymous'>
+        integrity='sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6' crossorigin='anonymous' />
+    <!-- Our styles -->
+    <link rel='stylesheet' href='css/index.css' />
+    <!-- icon -->
+    <link rel='icon' href='images/yuem-icon.jpg' />
     <title>yuem</title>
 </head>
 
 <body>
+    <!-- Archivo de inicio -->
+    <div id='logo-brackets'>
+        <div class='square'>
+            <img class='img' src='images/yuem.jpg' alt='YUEM'>
+        </div>
+        <b>By JuanMahecha9</b>
+    </div>
 
-<!-- Archivo de inicio -->
-<p>iniciando</p>
-
+    <!-- Fin -->
 </body>
 <!-- Boostrap js -->
 <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js'
@@ -79,19 +99,145 @@ cd scr/views && touch index.html && echo "
     crossorigin='anonymous'></script>
 
 </html>
+
+
 " >index.html
-cd .. && cd public && touch index.css
+cd .. && cd public && mkdir css && cd css && touch index.css
+echo "
+@import url('https://fonts.googleapis.com/css2?family=Bad+Script&family=Caveat:wght@700&family=Satisfy&family=Shadows+Into+Light+Two&display=swap');
+
+body {
+  text-align: center;
+  font-family: 'Shadows Into Light Two', cursive;
+  background-color: #0e0b16;
+}
+
+#logo-brackets {
+  width: 450px;
+  height: 400px;
+  position: relative;
+  background: #29abe2;
+  /* Old browsers */
+  background: -moz-linear-gradient(top, #29abe2 1%, #077cc3 100%);
+  /* FF3.6+ */
+  background: -webkit-gradient(linear, left top, left bottom, color-stop(1%, #29abe2), color-stop(100%, #077cc3));
+  /* Chrome,Safari4+ */
+  background: -webkit-linear-gradient(top, #29abe2 1%, #077cc3 100%);
+  /* Chrome10+,Safari5.1+ */
+  background: -o-linear-gradient(top, #29abe2 1%, #077cc3 100%);
+  /* Opera 11.10+ */
+  background: -ms-linear-gradient(top, #29abe2 1%, #077cc3 100%);
+  /* IE10+ */
+  background: linear-gradient(to bottom, #29abe2 1%, #077cc3 100%);
+  /* W3C */
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#29abe2', endColorstr='#077cc3',GradientType=0);
+  /* IE6-9 */
+  border-radius: 15% 15% 20% 20%;
+  margin: 3% auto;
+  text-align: center;
+  border-bottom: 30px solid #105991;
+}
+.square {
+  width: 75%;
+  height: 75%;
+  margin: 0 auto;
+  background: #fff;
+  border-radius: 10%;
+  position: relative;
+  top: 50px;
+  animation: cd-bounce-1 0.8s;
+}
+.square::after {
+  content: '{';
+  font-size: 270px;
+  font-weight: 800;
+  position: absolute;
+  left: 30px;
+  top: -50px;
+  color: #000;
+  animation: cd-bounce-1 2s;
+}
+.square::before {
+  content: '}';
+  font-size: 270px;
+  font-weight: 900;
+  position: absolute;
+  right: 30px;
+  top: -50px;
+  color: #000;
+  animation: cd-bounce-1 2s;
+}
+.square .img{
+  margin-top: 90px;
+  border-radius: 40px;
+  margin-left: 6.5px;
+}
+b {
+  color: #e7dfdd;
+  font-size: 50px;
+  position: relative;
+  top: 150px;
+}
+#logo-brackets {
+  visibility: hidden;
+}
+#logo-brackets {
+  visibility: visible;
+  animation: cd-bounce-1 1s;
+}
+@keyframes cd-bounce-1 {
+  0% {
+    opacity: 0;
+    transform: scale(1);
+  }
+  60% {
+    opacity: 1;
+    transform: scale(3);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+" > index.css
+cd ..
 cd ..
 cd ..
 #Devolver a la carpeta principal del proyecto
 
 #crear archivo GITIGNORE, para evitar que al crear u repositorio se suban archivos como los modulos de node
 touch .gitignore
-echo " .DS_STORE
+echo "
+.DS_STORE
 .idea/
 modules/.settings
 modules/.vscode
 node_modules
 # Ignore .history for the xyz.local-history VSCode extension
 .history
-"
+">.gitignore
+
+#Creación de las rutas iniciales
+cd src/routes && touch routes.js && echo "
+//Configuración de las rutas
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('index.html');
+});
+
+module.exports = router;
+"> routes.js
+
+#Descargar imagen y logo del programa
+cd ..
+cd public
+mkdir images
+cd images
+curl -O https://res.cloudinary.com/yuem1/image/upload/v1618457947/yuem.jpg
+curl -O https://res.cloudinary.com/yuem1/image/upload/v1618458569/yuem-icon.jpg
+cd ..
+cd ..
+cd ..
+#Volver a la carpeta principal del proyecto
--- readMe.md
+++ readMe.md
@@ -1,10 +1,34 @@
 YUEM by Juanmahecha9
 
+  
+
 ## ¿Que es?
+
+  
 
 Yuem es un proyecto que se enfoca en generar un proyecto generico de Node.js, en donde se enfoca en la optimizacion de tiempo para la creacion de pequeños proyectos.
 
+  
+
 ## ¿Cómo ejecutar?
 
+  
+
 Para sistemas Linux o MacOs, antes de ejecutar usar el comando `chmod +x nodeInit.sh`, el cual le dara los permisos necesarios para poder ejecutar el archivo.
+
 Al otorgar los permisos desde la terminal usar el comando `sh nodeInit` ó `./nodeInit.sh`, si se requiere ver que es lo que sucede internamente del programa durante la ejecución del programa usar el comando `sh -vx ./nodeInit.sh`.
+
+  
+
+## Ejecución del programa node.js
+
+  
+
+La ejecución del programa se ejecuta con los siguientes comandos:
+
+1. Enrutamiento a la carpeta del proyecto: `cd Nombre_asiganado_a_la_carpeta`.
+
+2. Ejecución del servidor; para esto se puede usar uno de los siguientes comandos:
+2.1. `node index` el cual ejecuta el servidor un unica vez.
+2.2. `npm start` el cual ejecuta el servidor un unica vez.
+2.3. `npm run start`el cual ejecuta el servidor bajo el modulo de nodemon, el cual vigila el sistema de archivos y reinicia automáticamente el proceso.
